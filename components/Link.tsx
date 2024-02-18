f6caee04eaca287c1ecaf35a7c2648ceb317335e
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-18 11:31:44 -0300

    style: add desktop styles to link component

--- components/Link.tsx
+++ components/Link.tsx
@@ -5,8 +5,8 @@ type LinkProps = {
 
 export default function Link({ children, type }: LinkProps) {
   const variants = {
-    header: "text-[13px] font-openSans text-veryDarkBlue",
-    main: "text-sm font-raleway text-moderateCyan border-b pr-5 bg-arrow bg-right bg-no-repeat border-moderateCyan m-auto",
+    header: "text-[13px] font-openSans text-veryDarkBlue text-sm",
+    main: "text-sm font-openSans text-moderateCyan border-b pr-5 bg-arrow bg-right bg-no-repeat border-moderateCyan m-auto lg:m-0 lg:text-lg lg:w-48",
     footer: "font-raleway text-lightGray font-semibold",
   };
 
