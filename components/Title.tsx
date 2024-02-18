9a97f511ead48010b53e9ef9cff38f24f011af89
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-17 23:48:22 -0300

    feat: add title component

---
+++ components/Title.tsx
@@ -0,0 +1,19 @@
+type TitleProps = {
+  as: "h1" | "h2";
+  className?: string;
+  children: string;
+};
+
+export default function Title({ as, className, children }: TitleProps) {
+  const Tag = as;
+  const variants =
+    as == "h1"
+      ? "text-2xl font-openSans text-veryDarkBlue"
+      : "text-[17px] font-raleway";
+
+  return (
+    <Tag className={`${className} ${variants} text-center font-bold`}>
+      {children}
+    </Tag>
+  );
+}
